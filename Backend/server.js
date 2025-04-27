6c9a47236fc9891ce43a8478ef7f7be384ba45e3
KRISHSANGWAN <129057379+KRISHSANGWAN@users.noreply.github.com>
2025-04-28 02:23:45 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ app.use(cors({
   origin: '*',     // Allow all origins TEMPORARILY (for now)
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization','token'],
+  allowedHeaders: ['Content-Type', 'Authorization','token','dToken', 'aToken'],
 }));
 app.use(express.json());
 
