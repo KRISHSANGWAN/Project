e04afde6e2e98bf4a6ee9a8c2dc939caa529f1d7
KRISHSANGWAN <129057379+KRISHSANGWAN@users.noreply.github.com>
2025-04-28 02:00:10 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,27 +1,33 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import adminRouter from "./routes/adminRoute.js";
 import doctorRouter from "./routes/doctorRoute.js";
 import userRouter from "./routes/userRoute.js";
-import cors from 'cors';
+
+// app configuration
 
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 connectCloudinary();
 
-// Allow all origins temporarily
-app.use(cors());
-app.options('*', cors());
-
+// Middlewares
+app.use(cors({
+  origin: '*',     // Allow all origins TEMPORARILY (for now)
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 
-// API Endpoints
-app.use("/api/admin", adminRouter);
-app.use("/api/doctor", doctorRouter);
-app.use("/api/user", userRouter);
+// API Endpoint
+
+app.use("/api/admin",adminRouter)
+app.use("/api/doctor",doctorRouter);
+app.use("/api/user",userRouter);
 
 app.get("/", (req, res) => {
   res.send("API is fine");
