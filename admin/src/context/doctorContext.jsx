242fc967b8285fefb78a9f993ff1a5babc0d1793
KRISHSANGWAN <129057379+KRISHSANGWAN@users.noreply.github.com>
2025-07-04 12:13:46 +0530

    Update doctorContext.jsx

--- admin/src/context/doctorContext.jsx
+++ admin/src/context/doctorContext.jsx
@@ -8,7 +8,7 @@ const DoctorContextProvider = (props) => {
 
     const backendurl = import.meta.env.VITE_BACKEND_URL;
 
-    const [dToken,setDToken] = useState(localStorage.getItem('dToken')?localStorage.getItem('dToken'):" ");
+    const [dToken,setDToken] = useState(localStorage.getItem('dToken')?localStorage.getItem('dToken'):"");
     const [appointments,setAppointments] = useState([]);
     const [dashData, setDashData] = useState(false);
     const [profileData, setProfileData] = useState(false);
